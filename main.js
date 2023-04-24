968a6f1f1212d3b376265f3d0b4ed56b5dedbfc2
IchadB <richardbetalmos@gmail.com>
2023-04-24 15:28:25 +0800

    Adding Constructor

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 
+// Users initialization
+const teachers = [];
+const students = [];
+
+//Teacher Constructor
+class Teacher{
+    constructor(id, firstName, lastName, gender, age){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+    }
+}
+
+//Student Constructor
+class Student{
+    constructor(id, firstName, lastName, gender, age){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+    }
+}
+
+
 // SIR FERDIE JS CODES STARTS HERE
 
 // SIR FERDIE JS CODES ENDS HERE
