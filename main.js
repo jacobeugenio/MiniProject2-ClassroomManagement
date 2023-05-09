b26bb25c5ded84f4b1de7ab1820b9bb5a357063f
IchadB <richardbetalmos@gmail.com>
2023-05-09 17:49:35 +0800

    added functions

--- main.js
+++ main.js
@@ -13,6 +13,14 @@
 
 // SIR RICHARD JS CODES STARTS HERE
 
+fetch('http://localhost:3000/teachers/get-teachers')
+.then(res => res.json())
+.then(data => {
+    const teacher = data.find(teacher => teacher.id === 0)
+    document.querySelector("#user_name").textContent = teacher.fname
+}).catch(error => console.log(error))
+
+
 // SIR RICHARD JS CODES ENDS HERE
 
 // Datatable loader script in dashboard - Student data
